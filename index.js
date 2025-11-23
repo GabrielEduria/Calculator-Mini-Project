a87523d90e2f4c440c76016bd5c1c26e133d833d
Gabriel Eduria <gabrielezekieleduria@gmail.com>
2025-11-24 06:14:12 +0800

    failed

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-const computer = () => {
-    
-}
+const display = document.getElementById("results");
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.value;
+
+        if (value === "AC"){
+            display.value = " ";
+            return;
+        }
+
+        if (value === "CE"){
+            display.value = display.value.slice(0, -1);
+            return;
+        }
+
+        if (value === "="){
+            try {
+                display.value = eval(display.value);
+                return;
+            } catch {
+                display.value = "Error";
+            }
+            return;
+        }
+
+        display.value += value;
+    });
+})
+// statement iterations of code, button arrow function, button addevent lisnster
+// dont forget to declare a value = button.value
+// AC = EMPTY
+// CE - 1 of line; slice() method
+// to get equal of value || try catch block;
+// clue eval function
+// diplay.value += value;
